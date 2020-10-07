144fbcb84a446c58edce8e799229d80844524fd7
Daniel Vargas <davarg5@yahoo.com>
2020-10-07 17:26:13 -0400

    finished MarsRover.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     const fetchImgUrls = () => {
       axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`)
         .then(res => {
-              setImgUrls(res.data.photos[0].img_src)
+          setImgUrls(res.data.photos[0].img_src);
         })
         .catch(err => {
           debugger
